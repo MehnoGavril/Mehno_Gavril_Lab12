a9a41aea6dd8dcc7ba2d2702104744db96573929
MIHAIL-GAVRIL MEHNO <gmehno@gmail.com>
2021-01-06 15:19:40 +0200

    Add project files.

---
+++ Mehno_Gavril_Lab12/Mehno_Gavril_Lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Mehno_Gavril_Lab12.Models;
+
+namespace Mehno_Gavril_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
---
+++ Mehno_Gavril_Lab12/Mehno_Gavril_Lab12/Models/ShopList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mehno_Gavril_Lab12.Models
+{
+    public class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
